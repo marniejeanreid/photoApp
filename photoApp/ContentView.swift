a6a80aaf9052bf0c384181762e44a8df6d5e29f1
Marnie Reid <marnie@kodewithklossy.com>
2024-04-18 12:47:05 -0400

    Complete Picture App

--- photoApp/ContentView.swift
+++ photoApp/ContentView.swift
@@ -2,20 +2,44 @@
 //  ContentView.swift
 //  photoApp
 //
-//  Created by scholar on 5/17/23.
+//  Created by scholar on 5/17/23.💚
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State var isImagePickerShowing = false
+    @State var selectedImage: UIImage?
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+                        
+            Image(uiImage: selectedImage ?? UIImage(named: "heartCamera")!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack(spacing: 50) {
+                Button("Select a Photo") {
+                    self.sourceType = .photoLibrary
+                    isImagePickerShowing = true
+                    
+                }
+                Button("Take a Photo") {
+                    self.sourceType = .camera
+                    isImagePickerShowing = true
+                    
+                }
+            }
+            
         }
-        .padding()
+        .sheet(isPresented: $isImagePickerShowing) {
+            ImagePicker(selectedImage: $selectedImage, isImagePickerShowing: $isImagePickerShowing, sourceType: self.sourceType)
+        }
+
     }
 }
 
